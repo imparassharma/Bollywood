fc2f416d613a37152ce7c8a1ff6b6ed523375c5f
Paras Sharma <parasangiras121@gmail.com>
2022-02-12 20:21:27 +0530

    commit

--- script.js
+++ script.js
@@ -1,25 +1,83 @@
+/******************************MOVIE************************************************/
 const movie = "DANGAL";
 const arr = Array.from(movie);
-console.log(arr)
 
+/******************************INPUT************************************************/
 function getVal(){
     const guess = document.getElementById("guess");
 }
 
+/*****************************MAIN SCRIPT************************************************/
+const result = document.querySelector(".result");
+const topsection = document.querySelector(".top-section");
+const midsection = document.querySelector(".mid-section");
+const bottomsection = document.querySelector(".bottom-section");
+const lives = document.getElementById("lives").children;
 const enter = document.getElementById("enter");
 
-var i=0,index=0;
+const index=[];
+let count=0,life=9,i=0;
+
 enter.addEventListener("click",function(){
     const input = guess.value;
-    console.log(input)
+    console.log(input);
+
+    const isInArray = arr.includes(input);
     for(i=0;i<arr.length;i++){
-        if(input==arr[i]){
-            const box = document.getElementById(i+1);
-            box.innerHTML = input;
+        if(arr[i]==input){
+            const pos = document.getElementById((i+1));
+            pos.innerHTML = input;
+            pos.parentElement.style.backgroundColor = "#17B978";
         }
-        else{
-            
-        }
+
     }
 
+    if(!arr.includes(input)){
+        life--;
+        lives[8-life].classList.add("wrong");
+    }
+
+    if(life==0){
+       result.classList.add("active");
+       midsection.classList.add("hidden");
+       bottomsection.classList.add("hidden");
+       topsection.classList.add("hidden");
+    }
+
+    
+
+})
+
+/************************************GAME LOST************************************************/
+
+const retry = document.getElementById("retry");
+const reveal = document.getElementById("reveal");
+
+retry.addEventListener("click",function(){
+
+})
+
+/*************************************GAME WON************************************************/
+
+
+
+
+
+/******************************COLOR SCHEME********************************************/
+const colors = document.querySelectorAll(".circle");
+const box = document.querySelector(".box");
+
+colors.forEach(function(color){
+    color.addEventListener("click",function(e){
+        console.log(e.currentTarget.style.backgroundColor);
+        const circle = e.currentTarget.className;
+        console.log(circle)
+        var current_color = circle.backgroundColor;
+        console.log(current_color)
+        colors.forEach(function(c){
+            c.classList.remove("current");
+        })
+        circle.classList.add("current");
+        box.classList.add("")
+    })
 })
